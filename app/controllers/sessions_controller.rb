6beabd3ed4e3e5294054b9e19d7107add99246ec
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-14 14:32:10 -0600

    authenticate_with_github_oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,14 @@ class SessionsController < ApplicationController
 		client_id = "0553da0f47202e004482"
 		client_secret = "1c9ba657839e676d92595b23aed80ec3d3dc77f9"
 		code = params[:code]
-
 		conn = Faraday.new(url: "https://github.com", headers: {"Accept": "application/json"})
 
 		response = conn.post("/login/oauth/access_token") do |req|
-			req.params["code"] = code
-			req.params["client_id"] = client_id
-			req.params["client_secret"] = client_secret
+			req.params = {
+				"code": code,
+				"client_id": client_id,
+				"client_secret": client_secret
+			}
 		end
 
 		data = JSON.parse(response.body, symbolize_names: true)
@@ -25,14 +26,24 @@ class SessionsController < ApplicationController
 		data = JSON.parse(response.body, symbolize_names: true)
 
 		user = User.find_or_create_by(uid: data[:id])
+
 		user.username = data[:login]
+
 		user.uid = data[:id]
+
 		user.token = access_token
+
 		user.save
+
 		session[:user_id] = user.id
-		require 'pry'; binding.pry
-		response = Faraday.get("https://api.github.com/user/repos", {"Authorization": "token #{user.token}"})
+
+		response = Faraday.get("https://api.github.com/user/repos", {
+			accept: "application/vnd.github+json", 
+			auth: "token #{user.token}"
+		})
+		
 		parsed = JSON.parse(response.body, symbolize_names: true)
-		# redirect_to dashboard_path
+		require 'pry'; binding.pry
+		redirect_to dashboard_path
 	end
 end
